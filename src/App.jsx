42e1d1b250bc13a1db8410ec297215f10d5d5b06
Marr <marat.salah1313@gmail.com>
2023-12-21 16:40:01 +0100

    add theme and img

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import NavBar from "./components/NavBar";
-import { ShowCase } from "./components/ShowCase";
+import { createTheme, ThemeProvider } from '@mui/material';
+import MyNavBar from './components/MyNavBar.jsx';
+import MediaCard from './components/MediaCard.jsx';
+import ShowCase from './components/ShowCase.jsx';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#111111',
+    },
+    secondary: {
+      main: '#F9DC31'
+    },
+  }
+});
 
 const App = () => {
+
   return (
-    <>
-      <NavBar></NavBar>
-      <ShowCase></ShowCase>
-    </>
+    <ThemeProvider theme={theme}>
+      <MyNavBar></MyNavBar>
+      <ShowCase MediaCard={MediaCard}></ShowCase>
+    </ThemeProvider>
   );
 };
 
 export default App;
+
