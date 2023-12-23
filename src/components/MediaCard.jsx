3eb11e7e4c8b3f9baaf1499cdf0a3eb9398e7bd1
Marr <marat.salah1313@gmail.com>
2023-12-23 15:14:27 +0100

    add description

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -24,7 +24,7 @@ export default function MediaCard(props) {
       <CardMedia
         sx={{ height: 140 }}
         image={img}
-        title="green iguana"
+        title="dog"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
